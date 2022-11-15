79c180d280c5eb6e927caea062afe11a6d4d94c2
Ali Shahab Rezaii <alisr18@uia.no>
2022-11-15 23:22:22 +0100

    First commit to GitHub

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace assignment_2.Controllers;
+
+public class CalculatorController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ wwwroot/js/calculator.js
@@ -0,0 +1,30 @@
+﻿let display = document.getElementById("display");
+
+let buttons = Array.from(document.getElementsByClassName("button"));
+console.log(buttons)
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case "C":
+                display.innerText = "";
+                break;
+            case "←":
+                display.innerText = display.innerText.slice(0, -1);
+                break;
+            case "=":
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error as";
+                }
+                break;
+            default:
+                if( (display.innerText.includes("+") || display.innerText.includes("-") || display.innerText.includes("*") || display.innerText.includes("/") ) && ( e.target.innerText === "+" || e.target.innerText === "-" || e.target.innerText === "*" || e.target.innerText === "/" )){
+                    return;
+                }
+                display.innerText += e.target.innerText;
+                   
+        }
+    });
+});
